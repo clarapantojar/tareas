8726ac73b99e54fb0129352958111277cf33c413
clarapantojar <017cpr@gmail.com>
2024-12-04 01:35:45 +0100

    hasta capitulo 7 - componentes livewire

---
+++ resources/views/livewire/counter-component.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <p>VISTA DEL COMPONENTE</p>
+    <p>El contador es {{ $count }}</p>
+
+    <button class="" wire:click="decrement">
+        Decrementar
+    </button>
+    <button class="" wire:click="increment">
+        Incrementar
+    </button>
+</div>
